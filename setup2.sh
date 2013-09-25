ddda412497fa0eb0191493c9ad1222c51067e482
C. Titus Brown <titus@idyll.org>
2013-09-25 12:39:52 -0400

    foo

--- setup2.sh
+++ setup2.sh
@@ -4,3 +4,4 @@ rm *.ipynb
 curl -O https://raw.github.com/ged-lab/2013-msu-zero-entry/master/graph-blast-bitscore.ipynb
 curl -O https://raw.github.com/ged-lab/2013-msu-zero-entry/master/filter-blast-csv.ipynb
 curl -O https://raw.github.com/ged-lab/2013-msu-zero-entry/master/monty-hall.ipynb
+curl -O https://raw.github.com/ged-lab/2013-msu-zero-entry/master/coin-flips.ipynb
