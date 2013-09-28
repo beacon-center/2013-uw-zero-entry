2ec2bab40c250715b912a8fe6f2ac35e07fdf297
C. Titus Brown <titus@idyll.org>
2013-09-28 08:50:43 -0700

    one single setup to rule them all

--- setup.sh
+++ setup.sh
@@ -17,3 +17,14 @@ echo 'cgi.assign = ( ".cgi" => "" )' >> 10-cgi.conf
 echo 'index-file.names += ( "index.cgi" ) ' >> 10-cgi.conf
 /etc/init.d/lighttpd restart
 
+pip install -U ipython
+pip install -U pyzmq
+
+cd /usr/local/notebooks
+rm *.ipynb
+curl -O https://raw.github.com/ged-lab/2013-msu-zero-entry/master/graph-blast-bitscore.ipynb
+curl -O https://raw.github.com/ged-lab/2013-msu-zero-entry/master/filter-blast-csv.ipynb
+curl -O https://raw.github.com/ged-lab/2013-msu-zero-entry/master/monty-hall.ipynb
+curl -O https://raw.github.com/ged-lab/2013-msu-zero-entry/master/coin-flips.ipynb
+
+shutdown -r now
